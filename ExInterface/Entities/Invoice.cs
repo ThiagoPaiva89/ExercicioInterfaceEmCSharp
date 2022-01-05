b120066f827e39660b2c0332ef753dc48a06c611
ThiagoPaiva89 <thiagoinaciopaiva@hotmail.com>
2022-01-05 15:01:45 -0300

    Exercicio Sem a Solucao de Interface Aplicada

---
+++ ExInterface/Entities/Invoice.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+namespace ExInterface.Entities
+{
+    class Invoice
+    {
+
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get
+            {
+                return BasicPayment + Tax;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Basic payment: "
+                + BasicPayment.ToString("f2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("f2", CultureInfo.InvariantCulture)
+                + "\nTotal: "
+                + TotalPayment.ToString("f2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
